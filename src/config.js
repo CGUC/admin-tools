3385936f0801dafed8e9bd4419c3d0af38533313
Ryan Martin <martr4929@hotmail.com>
2019-08-25 20:09:46 -0400

    Use correct auth server based on environment

--- src/config.js
+++ src/config.js
@@ -1,10 +1,14 @@
-const env = 'dev';
-
+const env = process.env.NODE_ENV;
 let AUTH_URL = 'https://skybunk-auth-dev.herokuapp.com';
-if (env === 'dev') {
-	AUTH_URL = 'https://skybunk-auth-dev.herokuapp.com'
-}
 
+if (env === 'development') {
+	AUTH_URL = 'https://skybunk-auth-dev.herokuapp.com'
+} else if (env === 'staging') {
+	AUTH_URL = 'https://skybunk-auth-staging.herokuapp.com'
+} else if (env === 'production') {
+	AUTH_URL = 'https://skybunk-auth-production.herokuapp.com'
+}
+console.log(env);
 let API_URL = '';
 function updateAPIUrl(url) {
 	API_URL = url;
