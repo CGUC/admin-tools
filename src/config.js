d4f5c6a35bdcce6c32275a3146f3157002d4468e
mipollex <40804290+mipollex@users.noreply.github.com>
2020-03-03 17:04:44 -0500

    Update config to pull from environment variables (#9)

--- src/config.js
+++ src/config.js
@@ -1,12 +1,4 @@
-let AUTH_URL = 'https://skybunk-auth-dev.herokuapp.com';
-
-if (process.env.NODE_ENV === 'development') {
-	AUTH_URL = 'https://skybunk-auth-dev.herokuapp.com'
-} else if (process.env.NODE_ENV === 'staging') {
-	AUTH_URL = 'https://skybunk-auth-staging.herokuapp.com'
-} else if (process.env.NODE_ENV === 'production') {
-	AUTH_URL = 'https://skybunk-auth-production.herokuapp.com'
-}
+const AUTH_URL = process.env.REACT_APP_AUTH_ADDRESS || 'https://skybunk-auth-production.herokuapp.com';
 
 export {
 	AUTH_URL
